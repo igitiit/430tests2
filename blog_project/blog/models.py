06b2490fba87b1378019cb1c3714d0a3f63954ba
Your Name <you@example.com>
2025-06-20 04:29:14 -0500

    Fixed database schema and removed author field

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)    
-    
-    def __str__(self):
-        return self.title
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+
+    class Meta:
+        db_table = 'blog_posts_v2'  # Changed table name
