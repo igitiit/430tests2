7cb2f4d6580634c9fa96e37f1659e1db754cf5d2
Your Name <you@example.com>
2025-06-20 04:57:21 -0500

    Update repository

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,12 +1,11 @@
+# blog/models.py
 from django.db import models
-from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
 
-    class Meta:
-        db_table = 'blog_post'  # Explicitly set to blog_post
+    def __str__(self):
+        return self.title
